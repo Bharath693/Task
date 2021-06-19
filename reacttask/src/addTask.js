93f88051c5d8dc83532b736622079e6ff5ee93de
bharath <bharathraj693@gmail.com>
2021-06-19 13:11:47 +0530

    Deleting task completed

--- reacttask/src/addTask.js
+++ reacttask/src/addTask.js
@@ -31,8 +31,29 @@ function AddTask(){
       .then(res=>res.json())
       .then(res=>setTask(res.results))
    },[])
- 
-   
+
+    function refreshpage(){
+        window.location.reload()
+    }
+
+    function DeleteTask(){
+      axios.delete("https://stage.api.sloovi.com/task/lead_6996a7dcdddc4af3b4f71ccb985cea38/task_773d44542f254b95acae8d32841df44b",{
+       method:"DELETE",
+       headers:{
+        'Authorization': 'Bearer ' + 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2MjQwMDYwMDUsIm5iZiI6MTYyNDAwNjAwNSwianRpIjoiM2Q0YWM2ZGYtMDc4OC00NjIwLWIxZDAtNDM4YjgxM2E2OTY4IiwiaWRlbnRpdHkiOnsibmFtZSI6IlN1YmkgU2lyIiwiZW1haWwiOiJzbWl0aGNoZXJ5bEB5YWhvby5jb20iLCJ1c2VyX2lkIjoidXNlcl82YmVlYzQ1OTkxNWY0NTA3YThkMjUyMGU2MGUwM2MzZSIsImNvbXBhbnlfaWQiOiJjb21wYW55XzNjNjhjZDk0ZWJkNjQ4Yzc4ZDc2ODcyY2ZhOWY4Y2ZiIiwiaWNvbiI6Imh0dHA6Ly93d3cuZ3JhdmF0YXIuY29tL2F2YXRhci9mMmU5YWNkZWM4MTdlMjRkMjk4MGQ4NTNlODkzODVmNT9kZWZhdWx0PWh0dHBzJTNBJTJGJTJGczMuc2xvb3ZpLmNvbSUyRmF2YXRhci1kZWZhdWx0LWljb24ucG5nIiwiYnlfZGVmYXVsdCI6Im91dHJlYWNoIn0sImZyZXNoIjpmYWxzZSwidHlwZSI6ImFjY2VzcyJ9.VB669RfV2l5RycCifHllIWTiMLBSPPbot2djEmoZrCE',
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+       }
+   })
+   .then((res) =>{
+       console.log(res)
+       refreshpage()
+   })
+   .then((err) =>{
+       console.log(err)
+   })
+}
+  
    
     return(
         <div>
@@ -81,7 +102,7 @@ function AddTask(){
                               <td>{item.task_msg}</td>
                               <td>{item.task_date}</td>
                               <td>{item.task_time}</td>
-                              <td><FontAwesomeIcon icon={faTrashAlt}/></td>
+                              <td><FontAwesomeIcon icon={faTrashAlt} onClick={DeleteTask}/></td>
                           </tr>
                           </>
                       )
@@ -91,4 +112,5 @@ function AddTask(){
         </div>
     )
 }
+
 export default AddTask
